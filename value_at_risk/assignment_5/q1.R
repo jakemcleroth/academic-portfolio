3fb29088d1690ddf754ee4cc6fce24d368a10328
jakemcleroth <119925475+jakemcleroth@users.noreply.github.com>
2025-09-21 19:42:34 +0200

    Add files via upload

---
+++ value_at_risk/assignment_5/q1.R
@@ -0,0 +1,139 @@
+###### Libraries ######
+library(readxl)
+library(dplyr)
+
+###### Functions ######
+ewma_beta <- function(index_returns, stock_returns, lambda) {
+  # This function assumes the first value of returns is NA
+  
+  # Check returns are the same length 
+  if (length(index_returns) != length(stock_returns)) {
+    stop("Index returns and stock returns have different lengths")
+  }
+  
+  # Initialize variables for EWMA calc
+  len <- length(index_returns)
+  ewma_var_index <- numeric(len)
+  ewma_covar <- numeric(len)
+  
+  # Use overall variance as starting values
+  ewma_var_index[1] <- var(index_returns[-1])
+  ewma_covar[1] <- cov(stock_returns[-1], index_returns[-1])
+  
+  # Calculate EWMA variances & covariance
+  for (i in 2:len) {
+    ewma_var_index[i] <- lambda * ewma_var_index[i - 1] + (1 - lambda) * index_returns[i]^2
+    ewma_covar[i] <- lambda * ewma_covar[i-1] + (1 - lambda) * stock_returns[i] * index_returns[i]
+  }
+  
+  # Calculate beta
+  ewma_beta <- ewma_covar[len]/ewma_var_index[len]
+  
+  return(c(ewma_var_index[len], ewma_beta))
+}
+
+ols_beta <- function(index_returns, stock_returns) {
+  return(cov(index_returns, stock_returns, use = "pairwise.complete.obs")/var(index_returns, na.rm = T))
+}
+
+###### Data ######
+data_daily <- read_excel("/Users/jakemcleroth/Desktop/University/Masters/Modules/Semester 1/VaR/assignment/assignment_5/data/q1.xlsx", sheet = "Daily")
+data_weekly <- read_excel("/Users/jakemcleroth/Desktop/University/Masters/Modules/Semester 1/VaR/assignment/assignment_5/data/q1.xlsx", sheet = "Weekly")
+
+data_daily <- data_daily %>% mutate(
+  index_returns = log(Index) - lag(log(Index), 1),
+  stock_returns = log(Stock) - lag(log(Stock), 1)
+)
+
+data_weekly <- data_weekly %>% mutate(
+  index_returns = log(Index) - lag(log(Index), 1),
+  stock_returns = log(Stock) - lag(log(Stock), 1)
+)
+
+
+###### Betas ######
+# a) OLS estimation using weekly data since 31 December 2001 (all data)
+beta_a <- ols_beta(index_returns = data_weekly$index_returns, stock_returns = data_weekly$stock_returns)
+# b) OLS estimation using weekly data since 28 December 2006
+beta_b <- ols_beta(index_returns = data_weekly[data_weekly$Date >= as.POSIXct("08/01/2016", format = "%d/%m/%Y", tz = "UTC"), ]$index_returns, stock_returns = data_weekly[data_weekly$Date >= as.POSIXct("08/01/2016", format = "%d/%m/%Y", tz = "UTC"), ]$stock_returns)
+# OLS estimation using daily data since 31 December 2001 (all data)
+beta_c <- ols_beta(index_returns = data_daily$index_returns, stock_returns = data_daily$stock_returns)
+# OLS estimation using daily data since 28 December 2006
+beta_d <- ols_beta(index_returns = data_daily[data_daily$Date >= as.POSIXct("08/01/2016", format = "%d/%m/%Y", tz = "UTC"), ]$index_returns, stock_returns = data_daily[data_daily$Date >= as.POSIXct("08/01/2016", format = "%d/%m/%Y", tz = "UTC"), ]$stock_returns)
+
+# EWMA estimation using weekly data with a smoothing constant of 0.95 (all data)
+e <- ewma_beta(index_returns = data_weekly$index_returns, stock_returns = data_weekly$stock_returns, lambda = 0.95)
+beta_e <- e[2]
+# EWMA estimation using weekly data with a smoothing constant of 0.9 (all data)
+f <- ewma_beta(index_returns = data_weekly$index_returns, stock_returns = data_weekly$stock_returns, lambda = 0.9)
+beta_f <- f[2]
+# EWMA estimation using daily data with a smoothing constant of 0.95 (all data)
+g <- ewma_beta(index_returns = data_daily$index_returns, stock_returns = data_daily$stock_returns, lambda = 0.95)
+beta_g <- g[2]
+# EWMA estimation using daily data with a smoothing constant of 0.9 (all data)
+h <- ewma_beta(index_returns = data_daily$index_returns, stock_returns = data_daily$stock_returns, lambda = 0.9)
+beta_h <- h[2]
+
+###### Yearly Vol ######
+vol_a <- sd(data_weekly$index_returns,na.rm = T)*sqrt(52)
+vol_b <- sd(data_weekly[data_weekly$Date >= as.POSIXct("08/01/2016", format = "%d/%m/%Y", tz = "UTC"), ]$index_returns, na.rm = T)*sqrt(52)
+vol_c <- sd(data_daily$index_returns,na.rm = T)*sqrt(250)
+vol_d <- sd(data_daily[data_daily$Date >= as.POSIXct("08/01/2016", format = "%d/%m/%Y", tz = "UTC"), ]$index_returns, na.rm = T)*sqrt(250)
+
+vol_e <- sqrt(e[1]*52)
+vol_f <- sqrt(f[1]*52)
+vol_g <- sqrt(g[1]*250)
+vol_h <- sqrt(h[1]*250)
+
+###### VaR ######
+var_a <- -qnorm(0.01)*beta_a*vol_a*sqrt(10/250)
+var_b <- -qnorm(0.01)*beta_b*vol_b*sqrt(10/250)
+var_c <- -qnorm(0.01)*beta_c*vol_c*sqrt(10/250)
+var_d <- -qnorm(0.01)*beta_d*vol_d*sqrt(10/250)
+var_e <- -qnorm(0.01)*beta_e*vol_e*sqrt(10/250)
+var_f <- -qnorm(0.01)*beta_f*vol_f*sqrt(10/250)
+var_g <- -qnorm(0.01)*beta_g*vol_g*sqrt(10/250)
+var_h <- -qnorm(0.01)*beta_h*vol_h*sqrt(10/250)
+
+###### Results Output ######
+### Beta estimates ###
+beta_a
+beta_b
+beta_c
+beta_d
+beta_e
+beta_f
+beta_g
+beta_h
+
+### Index Vol Estimate ###
+vol_a
+vol_b
+vol_c
+vol_d
+vol_e
+vol_f
+vol_g
+vol_h
+
+### VaR % ###
+var_a
+var_b
+var_c
+var_d
+var_e
+var_f
+var_g
+var_h
+
+### Rand VaR ###
+var_a*1000000
+var_b*1000000
+var_c*1000000
+var_d*1000000
+var_e*1000000
+var_f*1000000
+var_g*1000000
+var_h*1000000
+
+
---
+++ value_at_risk/assignment_5/q2_3_4.R
@@ -0,0 +1,209 @@
+###### Libraries ######
+library(readxl)
+library(dplyr)
+
+###### Data ######
+data <- read_excel("/Users/jakemcleroth/Desktop/University/Masters/Modules/Semester 1/VaR/assignment/assignment_5/data/q2_3_4.xlsx")
+data$Vol <- data$Vol*100
+
+###### Question 2 ######
+### Params ###
+n <- 2000
+point <- 100
+alpha <- 0.01
+
+### Returns ###
+data <- data %>% mutate(
+  index_returns = log(Index) - lag(log(Index), 1)
+)
+
+### Backtest ###
+data$VaR <- NA
+data$PnL <- NA
+data$Indicator_1 <- NA
+data$Indicator_2 <- NA
+data$Indicator_3 <- NA
+for (i in 252:2251) {
+  data$VaR[i] <- qnorm(alpha)*sd(data$index_returns[(i-250):(i-1)])*data$Index[i-1]*point
+  data$PnL[i] <- (data$Index[i] - data$Index[i-1])*point
+  data$Indicator_1[i] <- ifelse(data$VaR[i]>data$PnL[i],1,0)
+  
+}
+
+for (i in 253:2251) {
+  data$Indicator_2[i] <- ifelse(data$Indicator_1[i-1]==data$Indicator_1[i], ifelse(data$Indicator_1[i]==1,1,0),0)
+  data$Indicator_3[i] <- ifelse(data$Indicator_1[i-1]==data$Indicator_1[i], ifelse(data$Indicator_1[i]==0,1,0),0)
+}
+
+### Independence Test ###
+expected_exceptions = alpha*n
+n_1 <- sum(data$Indicator_1, na.rm = T)
+n_0 <- n-n_1
+
+n_00 <- sum(data$Indicator_3, na.rm = T)
+n_11 <- sum(data$Indicator_2, na.rm = T)
+n_01 <- n_1-n_11
+n_10 <- n_0-n_00
+p_exp <- alpha
+p_obs <- n_1/n
+p_01 <- n_01/(n_00+n_01)
+p_11 <- n_11/(n_10+n_11)
+
+numerator <- (p_obs^n_1) * ((1 - p_obs)^n_0)
+denominator <- (p_01^n_01) * ((1 - p_01)^n_00) * (p_11^n_11) * ((1 - p_11)^n_10)
+ln_lr <- ifelse(n_11>0, log(numerator / denominator), 0)
+test_stat <- -2*ln_lr
+
+if (qchisq(0.1,1,lower.tail = F)<test_stat) {
+  print("Reject at 0.1")
+} else {
+  print("Fail to Reject at 0.1")
+}
+
+if (qchisq(0.05,1,lower.tail = F)<test_stat) {
+  print("Reject at 0.05")
+} else {
+  print("Fail to Reject at 0.05")
+}
+
+if (qchisq(0.01,1,lower.tail = F)<test_stat) {
+  print("Reject at 0.01")
+} else {
+  print("Fail to Reject at 0.01")
+}
+
+
+###### Question 3 ######
+
+### Vol difference ###
+data <- data %>% mutate(
+  delta_vol = Vol - lag(Vol, 1)
+  
+)
+
+### Lagged vol difference ###
+data <- data %>% mutate(
+  lagged_delta_vol = lag(delta_vol,1)
+)
+
+### Fit regression ###
+model_full <- lm(Indicator_1 ~ lagged_delta_vol, data = data, na.action = na.omit)
+summary(model_full)
+data$null_model <- data$Indicator_1-alpha
+
+# RSS under H0
+RSS_restricted <- sum(data$null_model^2, na.rm = T)
+
+# RSS under full model
+RSS_full <- sum(resid(model_full)^2)
+
+# degrees of freedom
+df1 <- 2  
+df2 <- df.residual(model_full)
+
+# F-statistic
+F_stat <- ((RSS_restricted - RSS_full) / df1) / (RSS_full / df2)
+
+# p-value
+p_value <- pf(F_stat, df1, df2, lower.tail = FALSE)
+
+F_stat
+p_value
+
+if (p_value<0.05) {
+  print("Reject")
+} else {
+  print("Fail to Reject")
+}
+
+
+###### Question 4 ######
+rm(list = ls())
+
+
+data <- read_excel("/Users/jakemcleroth/Desktop/University/Masters/Modules/Semester 1/VaR/assignment/assignment_5/data/us_data_q2.xlsx")
+data$Vol <- data$Vol*100
+data <- data %>% mutate(
+  index_returns = log(Index) - lag(log(Index), 1)
+)
+
+### params ###
+lambda <- 0.94
+len <- nrow(data)
+alpha <- 0.01
+point <- 100
+n <- 2000
+test_prob <- 0.1
+chi_sqr_crit <- qchisq(test_prob,1,lower.tail = F)
+
+### EWMA Variance ###
+data$ewma_var_index <- NA
+data$ewma_var_index[1] <- var(data$index_returns[-1])
+data$ewma_var_index[3] <- lambda * data$ewma_var_index[1] + (1 - lambda) * data$index_returns[2]^2
+
+for (i in 4:len) {
+  data$ewma_var_index[i] <- lambda * data$ewma_var_index[i - 1] + (1 - lambda) * data$index_returns[i-1]^2
+}
+
+### Backtest ###
+data$VaR <- NA
+data$PnL <- NA
+data$Indicator_1 <- NA
+data$Indicator_2 <- NA
+data$Indicator_3 <- NA
+for (i in 252:2251) {
+  data$VaR[i] <- qnorm(alpha)*sqrt(data$ewma_var_index[i])*data$Index[i-1]*point
+  data$PnL[i] <- (data$Index[i] - data$Index[i-1])*point
+  data$Indicator_1[i] <- ifelse(data$VaR[i]>data$PnL[i],1,0)
+  
+}
+
+for (i in 253:2251) {
+  data$Indicator_2[i] <- ifelse(data$Indicator_1[i-1]==data$Indicator_1[i], ifelse(data$Indicator_1[i]==1,1,0),0)
+  data$Indicator_3[i] <- ifelse(data$Indicator_1[i-1]==data$Indicator_1[i], ifelse(data$Indicator_1[i]==0,1,0),0)
+}
+
+### Tests ###
+expected_exceptions = alpha*n
+n_1 <- sum(data$Indicator_1, na.rm = T)
+n_0 <- n-n_1
+
+n_00 <- sum(data$Indicator_3, na.rm = T)
+n_11 <- sum(data$Indicator_2, na.rm = T)
+n_01 <- n_1-n_11
+n_10 <- n_0-n_00
+p_exp <- alpha
+p_obs <- n_1/n
+p_01 <- n_01/(n_00+n_01)
+p_11 <- n_11/(n_10+n_11)
+
+# Unconditional Coverage
+numerator_uc <- (p_exp^n_1) * ((1 - p_exp)^n_0)
+denominator_uc <- (p_obs^n_1) * ((1 - p_obs)^n_0)
+ln_lr_uc <- log(numerator_uc / denominator_uc)
+test_stat_unc <- -2 * ln_lr_uc
+if (chi_sqr_crit<test_stat_unc) {
+  print("Reject")
+} else {
+  print("Fail to Reject")
+}
+
+# Independence
+numerator <- (p_obs^n_1) * ((1 - p_obs)^n_0)
+denominator <- (p_01^n_01) * ((1 - p_01)^n_00) * (p_11^n_11) * ((1 - p_11)^n_10)
+ln_lr <- ifelse(n_11>0, log(numerator / denominator), 0)
+test_stat_ind <- -2*ln_lr
+if (chi_sqr_crit<test_stat_ind) {
+  print("Reject")
+} else {
+  print("Fail to Reject")
+}
+
+# Conditional Coverage
+test_stat_con <- -2*(ln_lr+ln_lr_uc)
+if (qchisq(test_prob,2,lower.tail = F)<test_stat_con) {
+  print("Reject")
+} else {
+  print("Fail to Reject")
+}
+
